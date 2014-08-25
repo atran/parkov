020d4d1ed103dd9ccd5c5c4e856e2d8323a52109
Anthony Tran <atran.dummy@gmail.com>
2014-08-25 00:05:22 -0400

    wokring

--- app.py
+++ app.py
@@ -4,20 +4,16 @@ from pprint import pprint
 import numpy as np
 from numpy.random import random_sample
 from numpy.random.mtrand import dirichlet
-from numpy.random import choice
-import mad
-import pyaudio
+from random import choice
+from pyomxplayer import OMXPlayer
 import time
 
-p = pyaudio.PyAudio()
 knowledge = {}
 silent_knowledge = {}
+player = None
 
 def main():
-  assets_dir = './assets'
-  os.chdir(assets_dir)
-
-  sample_names = [mp3_file for mp3_file in glob("*.mp3")]
+  sample_names = [mp3_file for mp3_file in glob("assets/*.*")]
   playing_song = choice(sample_names)
 
   selector = ["silent", "silent", "sound"]
@@ -25,32 +21,33 @@ def main():
 
   knowledge = generateMarkovChain(sample_names)
   silent_knowledge = generateMarkovChain(selector)
-  
-  stream = p.open(format =
-                p.get_format_from_width(pyaudio.paInt32),
-                channels = 2,
-                rate = 44100,
-                output = True)
-  
+
+  player = OMXPlayer(playing_song, start_playback=True)
+
   while True:
-    silent_picker = pick_next(playing_selector, silent_knowledge)
-    next_step = silent_picker[0]
-
-    print("===============================================")
-    print("this round:")
-
-    if next_step == "silent":
-      next_prob = silent_picker[1]
-      play_silence(next_prob)
+    while player.finished == False:
+      continue
     else:
-      picker = pick_next(playing_song, knowledge)
-      next_song = picker[0]
-      next_prob = picker[1]
-      print("playing sound")
-      print("selected song: " + playing_song) 
-      print("selected song probability: " + str(next_prob))
-      play(next_song, stream)
-      playing_song = next_song
+      silent_picker = pick_next(playing_selector, silent_knowledge)
+      next_step = silent_picker[0]
+
+      print("===============================================")
+      print("this round:")
+
+      if next_step == "silent":
+        next_prob = silent_picker[1]
+        seconds_of_silence = float(next_prob*10)
+        print "playing silence for " + str(seconds_of_silence) + " seconds"
+        time.sleep(seconds_of_silence)
+      else:
+        picker = pick_next(playing_song, knowledge)
+        next_song = picker[0]
+        next_prob = picker[1]
+        print("playing sound")
+        print("selected song: " + playing_song) 
+        print("selected song probability: " + str(next_prob))
+        player = OMXPlayer(next_song, start_playback=True)    
+        playing_song = next_song
 
 def generateMarkovChain(sample_names):
   chain = {}
@@ -71,20 +68,6 @@ def pick_next(sample, markov):
   bins = np.add.accumulate(probabilities)
   song_index = np.digitize(random_sample(size), bins)
   return (values[song_index], probabilities[song_index])
-
-def play(filename, stream):
-  mf = mad.MadFile(filename)
-  data = mf.read()
-  while data != None:
-    stream.write(data)
-    data = mf.read()
-#  stream.close()
-#  p.terminate()
-
-def play_silence(prob):
-  seconds_of_silence = float(prob*10)
-  print "playing silence for " + str(seconds_of_silence) + " seconds"
-  time.sleep(seconds_of_silence)
 
 if __name__ == "__main__":
   main()
